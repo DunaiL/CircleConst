23d6b629c38946f361302399792e0c607994c9dd
root <root@v1645578.hosted-by-vdsina.ru>
2023-02-01 11:26:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0; // Координаты центра окружности. 
+            double r = 10; // Радиус окружности. 
+
+            Console.Write("Введите координаты точки: ");
+
+            x = Convert.ToDouble(Console.ReadLine()); // Ввод координат X. 
+
+            y = Convert.ToDouble(Console.ReadLine()); // Ввод координат Y.
+
+            if (Math.Sqrt(Math.Pow(x - 0, 2) + Math.Pow(y - 0, 2)) <= r) // Проверка, находятся ли указанные координаты (x;y) внутри указанной окружности (r).  
+                Console.WriteLine("Точка ({0};{1}) - попала!", x, y);
+            else
+                Console.WriteLine("Точка ({0};{1}) - не попалa!", x, y);
+
+            Console.ReadKey();
+        }
+    }
+}
